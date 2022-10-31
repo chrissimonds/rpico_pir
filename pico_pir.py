1c4b2973f924f242c5eb44ce3bdc8b52512e397a
Chris Simonds <32778284+chrissimonds@users.noreply.github.com>
2022-10-30 19:05:45 -0500

    Update pico_pir.py
    Lots of  code comment updates

--- pico_pir.py
+++ pico_pir.py
@@ -6,6 +6,9 @@ import time
 from time import sleep
 import sys
 
+# Print the Raspberry Pi Pico system information:
+#   Welcome, here is the system info:
+#   (name='micropython', version=(1, 19, 1), _machine='Raspberry Pi Pico with RP2040', _mpy=4358)
 
 print('Welcome, here is the system info:')
 print (str(sys.implementation))
@@ -19,13 +22,22 @@ timestring="%04d-%02d-%02d %02d:%02d:%02d"%(timestamp[0:3] + timestamp[4:7])
 
 print ('Date and Time = ' + timestring)
 
+
+#The program will use a logging file called data.txt which will need to be created before executing this code
+# You can create the file the first time from the command line with the following
+  # file = open("data.txt","w")
+  # file.close()
+# Please repeat commands above from command prompt to reset and empty file
+
 file = open("data.txt")
 file.read()
 file.write(timestring + ",Sys Info: " + str(sys.implementation) + "\n")
 file.close()
 
+
 # Read and print the CPU temperature 
 # sensor_temp = machine.ADC(4)
+
 sensor_temp = machine.ADC(machine.ADC.CORE_TEMP)
 conversion_factor = 3.3 / (65535)
 reading = sensor_temp.read_u16() * conversion_factor
@@ -35,20 +47,24 @@ print('Temperature = ' + str(temperature) + ' C  ' + str(fahrenheit) + ' F')
 
 
 # Onboard LED Setup
+# This is the new and preferred method that works with both Pico and Pico W models
 led = machine.Pin("LED", machine.Pin.OUT)
 led.off()
 
-
 # For the PIR sensor please remove jumper for best performance
 # Also dials may need to be adjusted but should not be
+# Experiment as needed with the PIR jumper settings and the sensitivy for desired motion triggering
 # Connect GP16 to PIR sensor's OUT pin), please use 3.3V connect to VCC on PIR sensor and GND to GND
+# Please remove the plastic shield to see the pin labels.
+# Connect the Pico's GP16 to PIR sensor's OUT pin.
+# Connect the Pico's 3.3V Pin to PIR sensor's OUT pin.
+# Connect the Pico's GND pin to PIR sensor's GND pin.
+
 pir = Pin(16, Pin.IN, Pin.PULL_UP)
 
+
+# Main program loop to write to data.txt
 while True:
-    # please create a new file the first time from the command line with the following
-    # file = open("data.txt","w")
-    # file.close()
-    # Please repeat commands above from command prompt to reset and empty file
     file = open("data.txt")
     file.read()
     file.write(timestring + ", Temp " + str(temperature) + "\n")
